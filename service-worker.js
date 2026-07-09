27cd93a5f9a8712b848014bdc2f1e15e45a512ec
indiamtm53-gif <indiamtm53@gmail.com>
2026-07-09 14:44:18 +0300

    v61.1 service worker eklendi

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = "ulkeden-haberler-v1";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
